3c5f2dbb42f430fc5a312386119c1282ec343dde
dev-ashishsethi <me.ashishsethi@gmail.com>
2021-11-20 00:54:27 +0530

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // https://api.funtranslations.com/translate/pirate.json
+
+let input= document.getElementsByClassName("text")[0];
+let btn= document.getElementsByClassName("btn-primary")[0];
+let output= document.getElementsByClassName("output")[0];
+
+function translate(){
+    let txt=input.value;
+    let url=`https://api.funtranslations.com/translate/pirate.json?text=${txt}`;
+    fetch(url).then(Response => Response.json()).then(
+        json => output.innerText= json.contents.translated);
+}
+
+btn.addEventListener("click",translate);
